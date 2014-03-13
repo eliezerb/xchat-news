42511e4eb425616cfc1ef2c27482a5fd9f25d5ee
Marlon Dauernheimer <marlon.dauernheimer@gmail.com>
2014-03-13 14:55:21 -0300

    Finalizado o plugin, para reconhecer a mensagem do chat e enviar a noticia

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*- 
-
+ # -*- coding: utf-8 -*-
 __module_name__ = "eNews - Plugin"
-__module_version__ = "0.1"
+__module_version__ = "0.0.1"
 __module_description__ = "Get random news from a RSS feed website"
 
 import httplib2
@@ -10,42 +9,54 @@ from sys import argv
 from BeautifulSoup import BeautifulStoneSoup
 from random import randint
 
-# Definindo a codificação padrão como UTF-8
-reload(sys)
-sys.setdefaultencoding("utf-8")
-
 # Esta linha é utilizada para receber um parametro no terminal indicando de qual site voce deseja visualizar as notícias
 # script, source = argv
 
-# Lista para indexar o nome dos servidores
-rss_servers_names = ['lifehacker', 'linux-journal', 'revista-info', 'gizmodo', 'lol-cats']
 
-# Dicionário com servidores e links
-rss_servers = {
-    'lifehacker': 'http://feeds.gawker.com/lifehacker/full.xml',
-    'linux-journal': 'http://feeds.feedburner.com/LinuxJournal-BreakingNews',
-    'revista-info': 'http://feeds.feedburner.com/Plantao-INFO',
-    'gizmodo': 'http://feeds.gawker.com/gizmodo/full',
-    'lol-cats': 'http://feeds.feedburner.com/lolcats/rss',
-}
+# Funcao que é chamada ao enviar um mensagem no chat
+def message_cb(word, word_eol, userdata):   
+    message = word[1]
+    #Verifica se ela e igual a !news
+    if(message == "!news"):
 
-# Inicia a bibilioteca http
-http = httplib2.Http()
+        # Lista para indexar o nome dos servidores
+        rss_servers_names = ['lifehacker', 'linux-journal', 'revista-info', 'gizmodo', 'lol-cats']
 
-# Realizar a requisicao no servidor escolhido aleatoriamente na lista de servidores
-#     - status -> cabecalho da requisicao
-#     - response -> corpo do arquivo XML
-status, response = http.request(rss_servers[rss_servers_names[randint(0, len(rss_servers_names)-1)]])
+        # Dicionário com servidores e links
+        rss_servers = {
+            'lifehacker': 'http://feeds.gawker.com/lifehacker/full.xml',
+            'linux-journal': 'http://feeds.feedburner.com/LinuxJournal-BreakingNews',
+            'revista-info': 'http://feeds.feedburner.com/Plantao-INFO',
+            'gizmodo': 'http://feeds.gawker.com/gizmodo/full',
+            'lol-cats': 'http://feeds.feedburner.com/lolcats/rss',
+        }
+       
 
-# Inicializa o soup com o conteúdo XML
-soup = BeautifulStoneSoup(response)
+        # Inicia a bibilioteca http
+        http = httplib2.Http()
 
-# Busca todos os itens dentro do XML, onde cada item representa uma noticia
-all_news = soup.findAll("item");
+        # Realizar a requisicao no servidor escolhido aleatoriamente na lista de servidores
+        # - status -> cabecalho da requisicao
+        # - response -> corpo do arquivo XML
+        status, response = http.request(rss_servers[rss_servers_names[randint(0, len(rss_servers_names)-1)]])
 
-# Seleciona aleatoriamente uma noticia presente na lista
-selected = randint(0, len(all_news)-1)
+        # Inicializa o soup com o conteúdo XML
+        soup = BeautifulStoneSoup(response)
 
-# Envia a mensagem no IRC
-xchat.command("ME "+all_news[selected].title.string + " - " + all_news[selected].link.string)
+        # Busca todos os itens dentro do XML, onde cada item representa uma noticia
+        all_news = soup.findAll("item");
+
+        #Recupera o inicio da mensagem ate o primeiro espaco em branco
+        message = word[1]
+   
+        # Seleciona aleatoriamente uma noticia presente na lista
+        selected = randint(0, len(all_news)-1)
+
+        # Envia a mensagem no IRC
+        xchat.command("ME "+all_news[selected].title.string + " - " + all_news[selected].link.string)       
+       
+xchat.prnt("Plugin carregado com sucesso");
+xchat.hook_print("Channel Message", message_cb)
+xchat.hook_print("Your Message", message_cb)
+xchat.hook_print("Private Message to Dialog", message_cb)
 
